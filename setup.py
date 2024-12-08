191f8debbb82ff5a06c593725d8d93d9be4c7425
Rahul Dixit <dev.rahuldixit@gmail.com>
2024-12-08 19:20:36 +0530

    Simplify project structure

--- setup.py
+++ setup.py
@@ -5,15 +5,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='pyfoil',
+    name='airfoilshapeopt',
     version='0.0.1',
-    author='Nathan A. Rooy',
-    author_email='nathanrooy@gmail.com',
-    url='https://github.com/nathanrooy/aerodynamic-shape-optimization',
+    author='Rahul Dixit, Pranati Bhatnagar, Ritik Kumar',
+    author_email= (
+        'dev.rahuldixit@gmail.com, '
+        'pranatib1234@gmail.com, '
+        'contactritik09@gmail.com'
+    ),
+    url='https://github.com/Neomech/aerodynamic-shape-optimization',
     description='A Python library for 2D airfoil analysis and shape optimization',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=['pyfoil'],
     python_requires='>=3.5',
     classifiers=[
         'Programming Language :: Python :: 3',
